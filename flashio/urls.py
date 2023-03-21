442d9799c5cde0ed36baca3ea6ef44c35c93eb23
Patryk <patrykprause@gmail.com>
2023-03-21 20:19:29 +0100

    statuc url

--- flashio/urls.py
+++ flashio/urls.py
@@ -66,7 +66,7 @@ urlpatterns = [
     # User management
     path("users/", include("users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 if settings.DEBUG:
