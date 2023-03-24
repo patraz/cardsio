eea0343b320f7a4591a9db27fac78309fb188348
Patryk <patrykprause@gmail.com>
2023-03-24 22:59:37 +0100

    modified:   flashcards/tasks.py

--- flashcards/tasks.py
+++ flashcards/tasks.py
@@ -48,11 +48,11 @@ def get_flashcards_from_prompt(amount, language, user_prompt, email):
 
 
     try:
-        f_cards = create_list_of_flashcards(d_old_str)
+        f_cards = create_list_of_flashcards(str(d_old_str))
     except:
         last_bracket_index = d_old_str.rfind(']')
         x = d_old_str[:last_bracket_index+1] + ']'
-        f_cards = create_list_of_flashcards(x)
+        f_cards = create_list_of_flashcards(str(x))
 
 
     deck = Deck.objects.create(name = user_prompt, list=f_cards, user=user)
