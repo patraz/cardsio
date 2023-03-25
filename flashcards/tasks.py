d1011794bba453b9807cc400f347f3dbd24395c2
Patryk <patrykprause@gmail.com>
2023-03-25 15:42:41 +0100

    dd

--- flashcards/tasks.py
+++ flashcards/tasks.py
@@ -49,12 +49,12 @@ def get_flashcards_from_prompt(amount, language, user_prompt, email):
 
     try:
         print("1 proba",  type(d_old_str))
-        f_cards = create_list_of_flashcards(str(d_old_str))
+        f_cards = create_list_of_flashcards(d_old_str)
     except:
         print("2 proba",  type(d_old_str))
         last_bracket_index = d_old_str.rfind(']')
         x = d_old_str[:last_bracket_index+1] + ']'
-        f_cards = create_list_of_flashcards(str(x))
+        f_cards = create_list_of_flashcards(x)
 
 
     deck = Deck.objects.create(name = user_prompt, list=f_cards, user=user)
