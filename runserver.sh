dc8ad30adf6dfbcf3b09b437bb093278169d9fe5
Patryk <patrykprause@gmail.com>
2023-07-20 23:03:47 +0200

    .

--- runserver.sh
+++ runserver.sh
@@ -1,7 +1,7 @@
 
 python manage.py migrate --fake
 python manage.py showmigrations
-manage.py migrate --run-syncdb
+python manage.py migrate --run-syncdb
 python manage.py makemigrations
 python manage.py migrate
 gunicorn flashio.wsgi --bind=0.0.0.0:80
