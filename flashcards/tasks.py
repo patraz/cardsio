a1ea9f59f4ba92ef2e12c4784cf82f4a47c8b27c
Patryk <patrykprause@gmail.com>
2024-04-28 09:35:22 +0200

    modified:   flashcards/tasks.py
     Changes not staged for commit:
    	modified:   flashio/settings.py
    	modified:   mydatabase

--- flashcards/tasks.py
+++ flashcards/tasks.py
@@ -17,14 +17,15 @@ from_email = settings.DEFAULT_FROM_EMAIL
 subject = '[Flashio] Your flashcards are ready'
 
 
-openai.api_key = settings.OPENAI_KEY 
+openai.api_key = settings.OPENAI_KEY
+
 
 @shared_task
 def get_flashcards_from_prompt(amount, language, user_prompt, email):
     user = User.objects.get(email=email)
-    
+
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-3.5-turbo-0301",
         messages=[
             {"role": "system", "content": "You are an expert that creates flashcards about given topic"},
             {"role": "user", "content": f"""
@@ -43,8 +44,7 @@ def get_flashcards_from_prompt(amount, language, user_prompt, email):
     user.save()
     resp = response["choices"][0]["message"].content
 
-    d_old_str = resp.replace('”','').replace('„','') # remove all \n
-
+    d_old_str = resp.replace('”', '').replace('„', '')  # remove all \n
 
     try:
         f_cards = create_list_of_flashcards(d_old_str)
@@ -53,8 +53,7 @@ def get_flashcards_from_prompt(amount, language, user_prompt, email):
         x = d_old_str[:last_bracket_index+1] + ']'
         f_cards = create_list_of_flashcards(x)
 
-
-    deck = Deck.objects.create(name = user_prompt, list=f_cards, user=user)
+    deck = Deck.objects.create(name=user_prompt, list=f_cards, user=user)
 
     for x in f_cards:
         Flashcard.objects.create(question=x[0], answer=x[1], deck=deck)
@@ -63,49 +62,52 @@ def get_flashcards_from_prompt(amount, language, user_prompt, email):
     send_mail(subject, message, from_email, recipient_list)
     return
 
+
 @shared_task
 def get_flashcards_from_text(subject, text, language, amount, email):
     user = User.objects.get(email=email)
-    text = text.replace('\n', '').replace('”','').replace('„','').replace("''", "").replace('""','')
+    text = text.replace('\n', '').replace('”', '').replace(
+        '„', '').replace("''", "").replace('""', '')
     if language == 'Polish':
         response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-16k",
-        messages=[
-            {"role": "system", "content": "Jesteś pomocnym asystentem i expertem, który tworzy flashcards na dany temat"},
-            {"role": "user", "content": f"""
+            model="gpt-3.5-turbo-16k-0613",
+            messages=[
+                {"role": "system", "content": "Jesteś pomocnym asystentem i expertem, który tworzy flashcards na dany temat"},
+                {"role": "user", "content": f"""
             Stwórz listę {amount} flashcards w języku {language} z tego tekstu: {text}.
             Only provide a  Python list compliant response  following this format without deviation. 
             [
             ["Question":"Answer"],
             ]"""}],
-        temperature=0.5,
-        max_tokens=5000,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
+            temperature=0.5,
+            max_tokens=5000,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
         )
     else:
         response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-16k",
-        messages=[
-            {"role": "system", "content": "You are an expert that creates flashcards anout given topic"},
-            {"role": "user", "content": f"""
+            model="gpt-3.5-turbo-16k-0613",
+            messages=[
+                {"role": "system", "content": "You are an expert that creates flashcards anout given topic"},
+                {"role": "user", "content": f"""
             Create a list of {amount} comprehensive flashcards in {language} from this text: {text}.
             Only provide a  Python list compliant response  following this format without deviation. 
             [
             ["Question":"Answer"],
             ]"""}],
-        temperature=0.5,
-        max_tokens=5000,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
+            temperature=0.5,
+            max_tokens=5000,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
         )
     user.point_balance = user.point_balance - amount*120
     user.save()
     resp = response["choices"][0]["message"].content
-    
-    d_old_str = resp.replace('\n', '').replace('”','').replace('„','') # remove all \n
+
+    d_old_str = resp.replace('\n', '').replace(
+        '”', '').replace('„', '')  # remove all \n
 
     try:
         f_cards = create_list_of_flashcards(str(d_old_str))
@@ -114,12 +116,11 @@ def get_flashcards_from_text(subject, text, language, amount, email):
         x = d_old_str[:last_bracket_index+1] + ']'
         f_cards = create_list_of_flashcards(str(x))
 
-    deck = Deck.objects.create(name = subject, list=f_cards, user=user)
+    deck = Deck.objects.create(name=subject, list=f_cards, user=user)
 
     for x in f_cards:
-                Flashcard.objects.create(question=x[0], answer=x[1], deck=deck)
+        Flashcard.objects.create(question=x[0], answer=x[1], deck=deck)
     recipient_list = [f'{email}']
     message = f'Flashio generated {len(f_cards)} flashcards about {subject}, check them at your dashboard'
     send_mail(subject, message, from_email, recipient_list)
-    return       
-
+    return
